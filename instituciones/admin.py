679b726f894606b19945f61bb6891126e54eeebb
villoch82 <villoch82@gmail.com>
2022-08-15 03:58:30 -0400

    adminsitracion

--- instituciones/admin.py
+++ instituciones/admin.py
@@ -1,6 +1,12 @@
+from dataclasses import field
+from django import forms
 from django.contrib import admin
 
 from instituciones.models import Instituciones
 
+class InstitucionesAdmin(admin.ModelAdmin):
+    fields = ['nombre', 'direccion', 'nif', 'fecha_creacion']
+    list_display = ['nombre', 'direccion', 'nif', 'fecha_creacion']
+
 # Register your models here.
-admin.site.register(Instituciones)
+admin.site.register(Instituciones, InstitucionesAdmin)
